266f92e6880c012a55dcd853ad747660fed58476
Peddamuthu <Q4C97NZ2@acsind.com>
2019-05-02 19:26:29 +0530

    Updates

---
+++ precache-manifest.b5cb59555d8348694806c02abdc1be88.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf261c46ae1a9a6fbb05c7efad2e02b",
+    "url": "/pruden-reads/index.html"
+  },
+  {
+    "revision": "0ddc12ac7fbc5d0ffae4",
+    "url": "/pruden-reads/static/css/main.a70c1939.chunk.css"
+  },
+  {
+    "revision": "4061206a451c2ffacccf",
+    "url": "/pruden-reads/static/js/2.f6b8c9bd.chunk.js"
+  },
+  {
+    "revision": "0ddc12ac7fbc5d0ffae4",
+    "url": "/pruden-reads/static/js/main.5c77570a.chunk.js"
+  },
+  {
+    "revision": "680f7035cae721dc52c0",
+    "url": "/pruden-reads/static/js/runtime~main.be22cbf7.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/pruden-reads/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/pruden-reads/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/pruden-reads/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/pruden-reads/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
